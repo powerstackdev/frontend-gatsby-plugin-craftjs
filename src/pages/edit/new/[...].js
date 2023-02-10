6c14fc7a82cee0f6506ae80ad593d7ae1c0efb9d
Xavier Mirabelli-Montan <xavier.mm@investisdigital.com>
2023-02-10 18:53:16 +0000

    Converting Craft.js example to Gatsby and theme ui format

--- src/pages/edit/new/[...].js
+++ src/pages/edit/new/[...].js
@@ -1,9 +1,15 @@
 /** @jsx jsx */
-import { jsx, Heading, Box, Card, Input, Button, Slider, Label, Flex, Textarea, Checkbox, Select, Radio } from "theme-ui"
-import React from "react"
-import { Tabs, Tab, TabList, TabPanel } from "react-tabs"
+import { Button, Card, jsx } from "theme-ui"
+import React, { useState } from "react"
 
-import Layout  from "gatsby-theme-drupal-admin/src/components/Layout/Layout"
+import { Editor, Element, Frame } from "@craftjs/core"
+
+import Layout from "gatsby-theme-drupal-admin/src/components/Layout/Layout"
+import { Section } from "../../../components/Section"
+import { Column } from "../../../components/Column"
+import { Sidebar } from "../../../components/Sidebar"
+import { Text } from "../../../components/Text"
+import { RenderNode } from "../../../components/RenderNode";
 
 /**
  * Page template to create a new CMS page with Tina inline blocks enabled
@@ -11,147 +17,85 @@ import Layout  from "gatsby-theme-drupal-admin/src/components/Layout/Layout"
  * @returns {JSX.Element}
  */
 const NewPage = () => {
+  const [canvasFullscreen, setCanvasFullscreen] = useState(false)
+
+  const toggleFullscreen = (event) => {
+    setCanvasFullscreen(!canvasFullscreen)
+  }
+
   return (
     <Layout isFull>
       <div
         sx={{
           display: "grid",
-          gridTemplateColumns: "1fr 4fr 1fr"
+          gridTemplateColumns: "1fr 4fr",
+          minHeight: "100%",
+          mt: 3,
         }}
       >
-        <aside sx={{
-          px: 4,
-          background: "white"
-        }}>
-          <Tabs>
-            <TabList sx={{
-              p: 0,
-              li: {
-                display: `inline-block`,
-                border: `1px solid transparent`,
-                borderBottom: `none`,
-                bottom: `-1px`,
-                position: `relative`,
-                listStyle: `none`,
-                padding: `6px 12px`,
-                cursor: `pointer`
-              }
-            }}>
-              <Tab>Blocks</Tab>
-              <Tab>Page settings</Tab>
-            </TabList>
-            <TabPanel>
-              <Heading>Blocks</Heading>
-              <Input placeholder="Search blocks..."/>
-              <Heading as="h3">Templates</Heading>
-              <Box sx={{
-                display: "grid",
-                gridTemplateColumns: ["1fr", "1fr", "1fr", "1fr 1fr"],
-                div: {
-                  m:2
-                }
-              }}>
-                <Card>
-                  Test
-                </Card>
-                <Card>
-                  Test 2
-                </Card>
-                <Card>
-                  Test 3
-                </Card>
-                <Card>
-                  Test 4
-                </Card>
-                <Card>
-                  Test 5
-                </Card>
-                <Card>
-                  Test 6
-                </Card>
-              </Box>
-              <Heading as="h3">Components</Heading>
-              <Box sx={{
-                display: "grid",
-                gridTemplateColumns: ["1fr", "1fr", "1fr", "1fr 1fr"],
-                div: {
-                  m:2
-                }
-              }}>
+        <Editor resolver={{ Text, Section, Column }} onRender={RenderNode}>
+          <Sidebar />
+          <main
+            sx={{
+              mx: 4,
+              height: "calc(100vh - 152px)",
+            }}
+          >
+            <Button
+              onClick={toggleFullscreen}
+              sx={{
+                position: `${canvasFullscreen && "fixed"}`,
+                top: `${canvasFullscreen && 3}`,
+                right: `${canvasFullscreen && 3}`,
+                zIndex: `${canvasFullscreen && 1002}`,
+              }}
+            >
+              {!canvasFullscreen ? "Fullscreen" : "Minimize"}
+            </Button>
+            <Card
+              sx={{
+                width: "100%",
+                height: "100%",
+                position: `${canvasFullscreen && "fixed"}`,
+                top: `${canvasFullscreen && 0}`,
+                left: `${canvasFullscreen && 0}`,
+                zIndex: `${canvasFullscreen && 1001}`,
+              }}
+              className={"page-container"}
+            >
+              <header
+                sx={{
+                  background: "primary",
+                  color: "white",
+                  p: 3,
+                }}
+              >
+                Header
+              </header>
+              <Frame>
+                <Element is={'div'} canvas>
+                  <Element id="sections" is={Section} canvas>
 
-                <Card>
-                  Test
-                </Card>
-                <Card>
-                  Test 2
-                </Card>
-                <Card>
-                  Test 3
-                </Card>
-                <Card>
-                  Test 4
-                </Card>
-                <Card>
-                  Test 5
-                </Card>
-                <Card>
-                  Test 6
-                </Card>
-              </Box>
-            </TabPanel>
-            <TabPanel>
-              <Heading>Page settings</Heading>
-              <Box as="form" onSubmit={(e) => e.preventDefault()}>
-                <Label htmlFor="username">Username</Label>
-                <Input name="username" id="username" mb={3} />
-                <Label htmlFor="password">Password</Label>
-                <Input type="password" name="password" id="password" mb={3} />
-                <Box>
-                  <Label mb={3}>
-                    <Checkbox />
-                    Remember me
-                  </Label>
-                </Box>
-                <Label htmlFor="sound">Sound</Label>
-                <Select name="sound" id="sound" mb={3}>
-                  <option>Beep</option>
-                  <option>Boop</option>
-                  <option>Blip</option>
-                </Select>
-                <Label htmlFor="comment">Comment</Label>
-                <Textarea name="comment" id="comment" rows={6} mb={3} />
-                <Flex mb={3}>
-                  <Label>
-                    <Radio name="letter" /> Alpha
-                  </Label>
-                  <Label>
-                    <Radio name="letter" /> Bravo
-                  </Label>
-                  <Label>
-                    <Radio name="letter" /> Charlie
-                  </Label>
-                </Flex>
-                <Label>Slider</Label>
-                <Slider mb={3} />
-                <Button>Submit</Button>
-              </Box>
-            </TabPanel>
-          </Tabs>
-        </aside>
-        <main>
-          Canvas
-        </main>
-        <aside>
-          Sidebar
-        </aside>
+                  </Element>
+                  <Text>Test text</Text>
+                  <Text>Test text 2</Text>
+                  <Text>Test text 3</Text>
+                </Element>
+              </Frame>
+              <footer>Site Footer</footer>
+            </Card>
+
+            <footer
+              sx={{
+                width: "100%",
+                bottom: 0,
+              }}
+            >
+              Footer
+            </footer>
+          </main>
+        </Editor>
       </div>
-      <footer
-        sx={{
-          width: "100%",
-        }}
-      >
-        Footer
-      </footer>
     </Layout>
   )
 }
